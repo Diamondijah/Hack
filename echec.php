9bb1c0e1b8cad91a65d9734ebd424d17aa58c410
Diamondijah <124451516+Diamondijah@users.noreply.github.com>
2023-06-22 10:46:15 +0000

    Add files via upload

---
+++ echec.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Échec de la connexion</title>
+</head>
+<body>
+    <h2>Échec de la connexion</h2>
+    <p>Les informations d'identification fournies sont invalides.</p>
+</body>
+</html>
